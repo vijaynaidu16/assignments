33e26a05fb1684d34d899097700c4c11580bedd8
vijay <digitalxvijay@gmail.com>
2023-12-25 18:22:56 +0530

    Week 3

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -14,7 +14,6 @@ app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
 });
-
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
@@ -22,5 +21,10 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(function(arr, req, res, next){
+  res.status(404).send()
+  errorCount++;
+})
 
 module.exports = app;
